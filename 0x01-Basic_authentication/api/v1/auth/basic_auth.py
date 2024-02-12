695e62173785c08c8a72c7ed48e233e58a040841
alidrisy <a.alidrisy6@gmail.com>
2024-02-12 21:37:31 +0300

    6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Model for th object BasicAuth """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Class to Authorized requests that inherits from Auth """
+    pass
