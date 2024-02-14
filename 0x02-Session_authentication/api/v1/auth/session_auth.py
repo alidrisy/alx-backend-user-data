916cb582ae4a62d2b8d5a2b61febdea4c08c0269
alidrisy <a.alidrisy6@gmail.com>
2024-02-14 18:50:56 +0300

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Model for th object SessionAuth """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class to Authorized sessions that inherits from Auth """
+    pass
