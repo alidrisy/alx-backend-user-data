d92bc5d4704690ec8c56efb35ada9e7373c37fe8
alidrisy <a.alidrisy6@gmail.com>
2024-02-21 18:59:09 +0300

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -34,4 +35,9 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """ Returns the first row found in the users table """
+        user = self._session.query(User).filter_by(**kwargs).one()
         return user
