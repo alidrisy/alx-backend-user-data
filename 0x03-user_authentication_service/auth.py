c417e67fee33e92b10fbe0443223813c8b2ef35f
alidrisy <a.alidrisy6@gmail.com>
2024-02-21 19:53:36 +0300

    3. update user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
-""" Model auth for _hash_password function """
+""" Auth Model """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ Create and return a salted hash of the input password """
     password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialize a new DB instance
+        """
+        self._db = DB()
+    
+    def register_user(self, email, password) -> User:
+        """ Create and return the User object """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            password = _hash_password(password)
+            return self._db.add_user(email, password)
